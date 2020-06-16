b0bc67093bd82916a96c57e52b56736fb2414947
Shawn Tschoepe <shawn.42496@gmail.com>
2020-06-15 22:30:59 -0500

    Have skeleton of basic gameplay

--- tripleX.cpp
+++ tripleX.cpp
@@ -4,18 +4,37 @@ int main()
 {
   std::cout << "You are a secret agent breaking into a secure server room...";
   std::cout << std::endl;
-  std::cout << "You need to enter the correct codes to continue...";
+  std::cout << "You need to enter the correct codes to continue..." << std::endl;
 
-  int a = 4;
-  int b = 7;
-  int c = 6;
+  const int CodeA = 4;
+  const int CodeB = 3;
+  const int CodeC = 2;
 
-  int sum = a + b + c;
-  int product = a * b * c;
+  const int CodeSum = CodeA + CodeB + CodeC;
+  const int CodeProduct = CodeA * CodeB * CodeC;
 
   std::cout << std::endl;
-  std::cout << sum << std::endl;
-  std::cout << product << std::endl;
+  std::cout << "+ There are 3 numbers in the code" << std::endl;
+  std::cout << "+ The codes add-up to: " << CodeSum << std::endl;
+  std::cout << "+ The codes multiply to give: " << CodeProduct << std::endl;
+
+  int GuessA, GuessB, GuessC;
+  std::cin >> GuessA;
+  std::cin >> GuessB;
+  std::cin >> GuessC;
+  std::cout << "You entered: " << GuessA << GuessB << GuessC;
+
+  int GuessSum = GuessA + GuessB + GuessC;
+  int GuessProduct = GuessA * GuessB * GuessC;
+
+  if (GuessSum == CodeSum && GuessProduct == CodeProduct)
+  {
+    std::cout << "You Win!";
+  }
+  else
+  {
+    std::cout << "You Lose!";
+  }
 
   return 0;
 }
